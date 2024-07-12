74666c002a115b402ccc26356dec30bc67a7b3fd
Erik <erik0304andersson@gmail.com>
2024-07-12 13:50:09 +0200

    feat: add solution

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -1,1 +1,2 @@
 pub mod prob1190;
+pub mod prob1717;
---
+++ src/tests/test1717.rs
@@ -0,0 +1,15 @@
+#[allow(unused_imports)]
+use crate::solutions::prob1717::Solution;
+
+
+#[test]
+fn test_reserve_parentheses() {
+    assert_eq!(
+        Solution::maximum_gain("cdbcbbaaabab".to_string(),4,5),
+        19
+    );
+    assert_eq!(
+        Solution::maximum_gain("aabbaaxybbaabb".to_string(),5,4),
+        20
+    );
+}
