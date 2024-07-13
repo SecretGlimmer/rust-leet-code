c55f481f5b552dc63321c69dbd0a4889f5df2a41
Erik <erik0304andersson@gmail.com>
2024-07-13 14:27:04 +0200

    feat: add solution

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod solutions;
 mod tests;
 
 fn main() {
-    println!("{}", solutions::prob1190::Solution::reserve_parentheses("(abcd)".to_string()));
-    println!("{}", solutions::prob1717::Solution::maximum_gain("aabbaaxybbaabb".to_string(),4,5));
+    // println!("{}", solutions::prob1190::Solution::reserve_parentheses("(abcd)".to_string()));
+    // println!("{}", solutions::prob1717::Solution::maximum_gain("aabbaaxybbaabb".to_string(),4,5));
+    let results = solutions::prob2751::Solution::survived_robots_healths(vec![3, 5, 2, 6], vec![10, 10, 15, 12], "RLRL".to_string());
+    println!("{:?}", results);
 }
---
+++ src/tests/test2751.rs
@@ -0,0 +1,19 @@
+#[allow(unused_imports)]
+use crate::solutions::prob2751::Solution;
+
+
+#[test]
+fn test_reserve_parentheses() {
+    assert_eq!(
+        Solution::survived_robots_healths(vec![5,4,3,2,1],vec![2,17,9,15,10],"RRRRR".to_string()),
+        vec![2,17,9,15,10]
+    );
+    assert_eq!(
+        Solution::survived_robots_healths(vec![3,5,2,6],vec![10,10,15,12],"RLRL".to_string()),
+        vec![14]
+    );
+    assert_eq!(
+        Solution::survived_robots_healths(vec![1,2,5,6],vec![10,10,11,11],"RLRL".to_string()),
+        vec![]
+    );
+}
