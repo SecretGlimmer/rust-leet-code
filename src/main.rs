bd0937fe72b5c1a2a564242cdb1c38b491fefe04
Erik <erik0304andersson@gmail.com>
2024-07-13 20:34:39 +0200

    fix: fix main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,12 @@ mod solutions;
 mod tests;
 
 fn main() {
-    // println!("{}", solutions::prob1190::Solution::reserve_parentheses("(abcd)".to_string()));
-    // println!("{}", solutions::prob1717::Solution::maximum_gain("aabbaaxybbaabb".to_string(),4,5));
-    let results = solutions::prob2751::Solution::survived_robots_healths(vec![3, 5, 2, 6], vec![10, 10, 15, 12], "RLRL".to_string());
-    println!("{:?}", results);
+    // let result = solutions::prob1190::Solution::reserve_parentheses("(abcd)".to_string());
+    // println!("{}", result);
+
+    // let result = solutions::prob1717::Solution::maximum_gain("aabbaaxybbaabb".to_string(),4,5);
+    // println!("{}", result);
+
+    // let results = solutions::prob2751::Solution::survived_robots_healths(vec![3, 5, 2, 6], vec![10, 10, 15, 12], "RLRL".to_string());
+    // println!("{:?}", results);
 }
